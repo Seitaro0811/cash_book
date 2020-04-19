fe80bb4d9f13f257c86613aaa04a70a279b5c542
seitaro.ogiri <seitaro.0811@icloud.com>
2020-04-20 05:26:01 +0900

    Modify index

--- WebContent/WEB-INF/views/cash_book/index.jsp
+++ WebContent/WEB-INF/views/cash_book/index.jsp
@@ -1,7 +1,55 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <c:import url="../layout/app.jsp">
     <c:param name="content">
-        <h2>おこづかい帳</h2>
+        <c:if test="${flush != null}">
+            <div id="flush_success">
+                <c:out value="${flush}"></c:out>
+            </div>
+        </c:if>
+        <h2><a href="">←</a> ____年 __月 <a href="">→</a></h2>
+        <table id="record_list" border="1">
+            <thead>
+                <tr>
+                    <th class="record_date">日付</th>
+                    <th class="record_content">内容</th>
+                    <th class="record_amount">金額</th>
+                    <th class="record_comment">コメント</th>
+                </tr>
+            </thead>
+            <tbody>
+                <c:forEach var="record" items="${records}">
+                    <tr>
+                        <td class="record_date"><fmt:formatDate value="${record.date}" pattern="yyyy-MM-dd" /></td>
+                        <td class="record_content">${record.content}</td>
+                        <td class="record_amount">${record.amount}</td>
+                        <td class="record_comment"><c:out value="${record.comment}" /></td>
+                    </tr>
+                </c:forEach>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>総支出</td>
+                    <td>残金</td>
+                </tr>
+            </tfoot>
+        </table>
+
+        <div id="pagination">
+            (全 ${records_count} 件)<br />
+            <c:forEach var="i" begin="1" end="${(records_count - 1) / 15 + 1}" step="1">
+                <c:choose>
+                    <c:when test="${i == page}">
+                        <c:out value="${i}" />&nbsp;
+                    </c:when>
+                    <c:otherwise>
+                        <a href="<c:url value='/?page=${1}' />"><c:out value="${i}" /></a>&nbsp;
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+        </div>
     </c:param>
 </c:import>
--- src/cash_book/IndexServlet.java
+++ src/cash_book/IndexServlet.java
@@ -1,13 +1,19 @@
 package cash_book;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.BookUser;
+import models.Record;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -28,7 +34,36 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //EntityManager em = DBUtil.createEntityManager();
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(Exception e) {
+            page = 1;
+        }
+        BookUser login_user = (BookUser)request.getSession().getAttribute("login_user");
+
+        List<Record> records = em.createNamedQuery("getAllRecords", Record.class)
+                                    .setParameter("user", login_user)
+                                    .setFirstResult(15 * (page - 1))
+                                    .setMaxResults(15)
+                                    .getResultList();
+
+        long records_count = (long)em.createNamedQuery("getRecordsCount", Long.class)
+                                        .setParameter("user", login_user)
+                                        .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("records", records);
+        request.setAttribute("records_count", records_count);
+        request.setAttribute("page", page);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/cash_book/index.jsp");
         rd.forward(request, response);
     }
