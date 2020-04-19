4c6638cba00a14c1850d7cc685e4794a42c9228b
seitaro.ogiri <seitaro.0811@icloud.com>
2020-04-20 05:25:23 +0900

    Add Record model

---
+++ src/models/Record.java
@@ -0,0 +1,100 @@
+package models;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name ="records")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllRecords",
+            query = "SELECT r FROM Record AS r WHERE r.book_user = :user ORDER BY r.date DESC"
+            ),
+    @NamedQuery(
+            name = "getRecordsCount",
+            query = "SELECT COUNT(r) FROM Record AS r WHERE r.book_user = :user"
+            )
+})
+@Entity
+public class Record {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_user_id", nullable = false)
+    private BookUser book_user;
+
+    @Column(name = "date", nullable = false)
+    private Calendar date;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Lob
+    @Column(name = "comment", nullable = true)
+    private String comment;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BookUser getBook_user() {
+        return book_user;
+    }
+
+    public void setBook_user(BookUser book_user) {
+        this.book_user = book_user;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+}
---
+++ src/models/validators/RecordValidator.java
@@ -0,0 +1,51 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import models.Record;
+
+public class RecordValidator {
+    public static List<String> validate(Record r) {
+        List<String> errors = new ArrayList<String>();
+
+        String date_error = _validateDate(r.getDate());
+        if(!date_error.equals("")) {
+            errors.add(date_error);
+        }
+
+        String content_error = _validateContent(r.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        String amount_error = _validateAmount(r.getAmount());
+        if(!amount_error.equals("")) {
+            errors.add(amount_error);
+        }
+
+        return errors;
+    }
+
+    private static String _validateDate(Calendar date) {
+        if(date == null || date.equals("")) {
+            return "日付を入力してください。";
+        }
+        return "";
+    }
+
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "支出入区分を入力してください。";
+        }
+        return "";
+    }
+
+    private static String _validateAmount(Integer amount) {
+        if(amount == null) {
+            return "金額を入力してください。";
+        }
+        return "";
+    }
+}
